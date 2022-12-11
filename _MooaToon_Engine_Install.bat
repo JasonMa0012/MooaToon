7165801da7841939816ccd4b3427dcf6d113ea74
Jason Ma <1312119064@qq.com>
2022-12-12 02:17:57 +0800

    update install tool

---
+++ _MooaToon_Engine_Install.bat
@@ -0,0 +1,9 @@
+
+cd MooaToon-Engine
+git clone https://github.com/Jason-Ma-0012/MooaToon-Engine.git
+
+Setup.bat
+GenerateProjectFiles.bat
+UE5.sln
+
+pause
---
+++ _MooaToon_Project_Install.bat
@@ -0,0 +1,5 @@
+
+cd MooaToon-Project
+git clone -b 5.1_MooaToonProject https://github.com/Jason-Ma-0012/MooaToon-Engine.git
+
+pause
