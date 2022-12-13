83910ffad82c335cfe4c158f2e727255e54eb9a7
Jason Ma <1312119064@qq.com>
2022-12-13 17:34:42 +0800

    update install tool

--- _2_3_Setup_Unreal_Engine.bat
+++ _2_3_Setup_Unreal_Engine.bat
@@ -1,4 +1,19 @@
+@echo off
+
+setlocal enabledelayedexpansion
 
 set repoName=MooaToon-Engine
 
-%repoName%\Setup.bat
+cd %repoName%
+
+:loop
+Setup.bat
+if not %errorlevel% == 0 (
+   echo Setup failed, retrying in 3 seconds...
+   timeout /t 3 /nobreak
+   goto loop
+)
+
+echo Setup successful
+
+timeout /t 5
