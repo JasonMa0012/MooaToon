3a0da82f277b1e32646d3924e11ae6d3174c753c
JasonMa <1312119064@qq.com>
2022-12-14 20:29:24 +0800

    update install tool

--- _4_1_Build_MooaToon_Engine.bat
+++ _4_1_Build_MooaToon_Engine.bat
@@ -2,9 +2,6 @@ rem @echo off
 
 set repoName=MooaToon-Engine
 
-%repoName%\GenerateProjectFiles.bat
-timeout /t 5
+call %repoName%\GenerateProjectFiles.bat
 
-%repoName%\Engine\Build\BatchFiles\Build.bat -Target="UnrealEditor Win64 Development" -Target="ShaderCompileWorker Win64 Development -Quiet" -WaitMutex -FromMsBuild
-
-timeout /t 5
+call %repoName%\Engine\Build\BatchFiles\Build.bat -Target="UnrealEditor Win64 Development" -Target="ShaderCompileWorker Win64 Development -Quiet" -WaitMutex -FromMsBuild
