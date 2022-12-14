2e713fca49c3038240f61bffca250788472e302b
Jason Ma <1312119064@qq.com>
2022-12-14 21:46:34 +0800

    fix install vs

--- _1_2_Install_VisualStudio.bat
+++ _1_2_Install_VisualStudio.bat
@@ -4,7 +4,7 @@ cd InstallTools
 echo Installing Visual Studio 2022...
 
 
-vs_Community.exe --wait --nocache --addProductLang En-us --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Workload.NativeGame --add Microsoft.VisualStudio.Workload.Universal --add Component.Unreal.Ide --add Component.Unreal 
+vs_Community.exe --wait --nocache --addProductLang En-us --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Workload.NativeGame --add Microsoft.VisualStudio.Workload.Universal --add Component.Unreal.Ide --add Component.Unreal --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64
 
 rem --quiet --norestart
 rem --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 
