e02fe9484198b60741fd5756102eeacb495e46c8
JasonMa <1312119064@qq.com>
2022-12-18 02:20:19 +0800

    fix dir

--- _3_3_Force_Update_MooaToon_Engine.bat
+++ _3_3_Force_Update_MooaToon_Engine.bat
@@ -5,6 +5,7 @@ set repoURL=https://github.com/Jason-Ma-0012/MooaToon-Engine.git
 set repoName=MooaToon-Engine
 set branchName=5.1
 
+cd %repoName%
 
 echo ===================================================
 git status -s | find "."
