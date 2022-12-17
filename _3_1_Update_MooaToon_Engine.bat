8bb4ca9620fe9b8edd7f8499966c8a66b8994c8e
Jason Ma <1312119064@qq.com>
2022-12-18 02:14:53 +0800

    add git Merge

--- _3_1_Update_MooaToon_Engine.bat
+++ _3_1_Update_MooaToon_Engine.bat
@@ -1,38 +1,19 @@
 @echo off
 
-setlocal enabledelayedexpansion
 
 set repoURL=https://github.com/Jason-Ma-0012/MooaToon-Engine.git
 set repoName=MooaToon-Engine
 set branchName=5.1
 
 
-if not exist %repoName% mkdir %repoName%
-cd %repoName%
+git checkout %branchName%
+git pull origin %branchName%
+git merge origin %branchName%
 
-git init
-
-:loop
-git fetch --depth=1 %repoURL% %branchName%
 if not %errorlevel% == 0 (
-   echo Fetch failed, retrying in 5 seconds...
-   timeout /t 5 /nobreak
-   goto loop
+    echo Merge failed, please run Force Update after backup files.
+    pause
+)else (
+    echo %repoName% successfully update.
+    timeout /t 5
 )
-
-echo ===================================================
-git status -s | find "M "
-echo ===================================================
-if %errorlevel% == 0 (
-   color 04
-   set /p input=You have modified files, discard and continue?  [Enter]
-)
-
-git reset --hard
-git checkout -b %branchName%_temp FETCH_HEAD
-git branch -D %branchName%
-git branch -m %branchName%_temp %branchName%
-
-
-echo %repoName% successfully update.
-timeout /t 5
---
+++ _3_4_Force_Update_MooaToon_Project.bat
@@ -0,0 +1,24 @@
+@echo off
+
+
+set repoURL=https://github.com/Jason-Ma-0012/MooaToon-Engine.git
+set repoName=MooaToon-Project
+set branchName=5.1_MooaToonProject
+
+
+echo ===================================================
+git status -s | find "."
+echo ===================================================
+if %errorlevel% == 0 (
+   color 04
+   set /p input=You have modified files, discard and continue?  [Enter]
+)
+
+git reset --hard
+git checkout -b %branchName%_temp FETCH_HEAD
+git branch -D %branchName%
+git branch -m %branchName%_temp %branchName%
+
+
+echo %repoName% successfully update.
+timeout /t 5
