0129c412e4f3cc5cc7e3320e9ce7ef6a1f6958d8
JasonMa <jasonma0012@foxmail.com>
2023-03-26 21:51:48 +0800

    fix: install tool

--- _3_5_Update_InstallTool.bat
+++ _3_5_Update_InstallTool.bat
@@ -6,9 +6,19 @@ set branchName=main
 
 git init
 
+:loop
+git fetch --depth=1 %repoURL% %branchName%
+if not %errorlevel% == 0 (
+   echo Fetch failed, retrying in 5 seconds...
+   timeout /t 10 /nobreak
+   goto loop
+)
+
+git checkout FETCH_HEAD
+git remote add origin %repoURL%
+git pull origin %branchName%
 git checkout %branchName%
-git pull --depth=50 origin %branchName%
-git merge origin/%branchName%
+git reset --hard origin/%branchName%
 
 echo successfully update.
 timeout /t 10
