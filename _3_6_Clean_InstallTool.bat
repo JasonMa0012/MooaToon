a2950d96b819205cd9143248b37658d4503ed5c4
JasonMa <1312119064@qq.com>
2023-07-04 01:58:17 +0800

    fix bugs

--- _3_6_Clean_InstallTool.bat
+++ _3_6_Clean_InstallTool.bat
@@ -2,33 +2,16 @@
 
 setlocal enabledelayedexpansion
 
-echo ^============================================================
-echo ==               Updating Installation Tool               ==
-echo ============================================================^
-
-
-git init
-
-
-set repoURL=https://github.com/JasonMa0012/MooaToon.git
-set branchName=main
-set PROMPT_ARGUMENT=--prompt --threads=16
-
-
-
-:loop
-git fetch --depth=50 %repoURL% %branchName%
-if not %errorlevel% == 0 (
-   echo Fetch failed, retrying in 5 seconds...
-   timeout /t 10 /nobreak
-   goto loop
+echo ===================================================
+git status -s | find "."
+echo ===================================================
+if %errorlevel% == 0 (
+   color 04
+   set /p input=You have modified files, discard and continue?  [Enter]
 )
 
-git checkout FETCH_HEAD
-git remote add origin %repoURL%
-git pull origin %branchName%
-git checkout %branchName%
-git reset --hard origin/%branchName%
+git reset --hard
+git clean -df
 
-echo successfully update.
+echo successfully clean.
 timeout /t 10
