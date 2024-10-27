54d616966ef2f6e42a80dee81cce6f680031a1bb
Jason Ma <1312119064@qq.com>
2024-10-27 13:50:19 +0800

    Update

--- _1_2_Install_VisualStudio.bat
+++ _1_2_Install_VisualStudio.bat
@@ -7,8 +7,19 @@ echo Installing Visual Studio 2022...
 rem https://learn.microsoft.com/zh-cn/visualstudio/install/workload-component-id-vs-community?view=vs-2022
 rem https://learn.microsoft.com/zh-cn/visualstudio/install/use-command-line-parameters-to-install-visual-studio?view=vs-2022
 
-vs_Community.exe --wait --addProductLang En-us --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Workload.NativeGame --add Microsoft.VisualStudio.Workload.Universal --add Microsoft.NetCore.Component.Runtime.6.0 --add Microsoft.Net.Component.4.6.2.TargetingPack --add Microsoft.VisualStudio.Component.VC.14.38.17.8.x86.x64 --add Microsoft.VisualStudio.Component.VC.14.38.17.8.x86.x64.Spectre --add Microsoft.VisualStudio.Component.VC.14.38.17.8.CLI.Support --add Microsoft.VisualStudio.Component.VC.14.38.17.8.ATL --add Microsoft.VisualStudio.Component.VC.14.38.17.8.MFC --add Microsoft.VisualStudio.Component.Windows10SDK.18362
-
+vs_Community.exe --wait --addProductLang En-us ^
+ --add Microsoft.VisualStudio.Component.Windows10SDK.18362 ^
+ --add Microsoft.VisualStudio.Component.VC.14.38.17.8.x86.x64 ^
+ --add Microsoft.VisualStudio.Component.VC.14.38.17.8.x86.x64.Spectre ^
+ --add Microsoft.VisualStudio.Component.VC.14.38.17.8.CLI.Support ^
+ --add Microsoft.VisualStudio.Component.VC.14.38.17.8.ATL ^
+ --add Microsoft.VisualStudio.Component.VC.14.38.17.8.MFC ^
+ --add Microsoft.VisualStudio.Workload.ManagedDesktop ^
+ --add Microsoft.VisualStudio.Workload.NativeDesktop ^
+ --add Microsoft.VisualStudio.Workload.NativeGame ^
+ --add Microsoft.VisualStudio.Workload.Universal ^
+ --add Microsoft.NetCore.Component.Runtime.6.0 ^
+ --add Microsoft.Net.Component.4.6.2.TargetingPack
 
 
 rem --quiet --norestart
