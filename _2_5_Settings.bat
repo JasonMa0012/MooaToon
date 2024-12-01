e8021fe7d1193c8d10c2efda17ce54ef37634c1c
Jason Ma <1312119064@qq.com>
2024-12-01 17:46:15 +0800

    Init 5.5

--- _2_5_Settings.bat
+++ _2_5_Settings.bat
@@ -1,7 +1,7 @@
 set repoURL=https://github.com/Jason-Ma-0012/MooaToon-Engine.git
 set engineFolderName=MooaToon-Engine
-set engineBranchName=5.4
+set engineBranchName=5.5
 set projectFolderName=MooaToon-Project
-set projectBranchName=5.4_MooaToonProject
+set projectBranchName=5.5_MooaToonProject
 set PROMPT_ARGUMENT=--prompt --threads=16
 
