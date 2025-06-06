f83d7553cba243456ceee67aeb272962185163fd
Jason Ma <jasonma0012@foxmail.com>
2025-06-07 01:42:39 +0800

    Bump to 5.6

--- _2_5_Settings.bat
+++ _2_5_Settings.bat
@@ -1,7 +1,7 @@
 set repoURL=https://github.com/Jason-Ma-0012/MooaToon-Engine.git
 set engineFolderName=MooaToon-Engine
-set engineBranchName=5.5
+set engineBranchName=5.6
 set projectFolderName=MooaToon-Project
-set projectBranchName=5.5_MooaToonProject
+set projectBranchName=5.6_MooaToonProject
 set PROMPT_ARGUMENT=--prompt --threads=16
 
