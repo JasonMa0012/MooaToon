ec013269170b36680da254d78b78127139812f7e
Jason Ma <jasonma0012@foxmail.com>
2025-12-07 00:06:12 +0800

    Bump to 5.7

--- _1_2_Install_VisualStudio.bat
+++ _1_2_Install_VisualStudio.bat
@@ -6,20 +6,20 @@ echo Installing Visual Studio 2022...
 
 rem https://learn.microsoft.com/zh-cn/visualstudio/install/workload-component-id-vs-community?view=vs-2022
 rem https://learn.microsoft.com/zh-cn/visualstudio/install/use-command-line-parameters-to-install-visual-studio?view=vs-2022
+rem https://dev.epicgames.com/documentation/zh-cn/unreal-engine/unreal-engine-5-7-release-notes#platform-sdk-upgrades
 
 vs_Community.exe --wait --addProductLang En-us ^
- --add Microsoft.VisualStudio.Component.Windows11SDK.22621 ^
- --add Microsoft.VisualStudio.Component.VC.14.38.17.8.x86.x64 ^
- --add Microsoft.VisualStudio.Component.VC.14.38.17.8.x86.x64.Spectre ^
- --add Microsoft.VisualStudio.Component.VC.14.38.17.8.CLI.Support ^
- --add Microsoft.VisualStudio.Component.VC.14.38.17.8.ATL ^
- --add Microsoft.VisualStudio.Component.VC.14.38.17.8.MFC ^
  --add Microsoft.VisualStudio.Workload.ManagedDesktop ^
  --add Microsoft.VisualStudio.Workload.NativeDesktop ^
  --add Microsoft.VisualStudio.Workload.NativeGame ^
  --add Microsoft.VisualStudio.Workload.Universal ^
- --add Microsoft.NetCore.Component.Runtime.6.0 ^
- --add Microsoft.Net.Component.4.6.2.TargetingPack
+ --add Microsoft.VisualStudio.Component.Windows11SDK.22621 ^
+ --add Microsoft.VisualStudio.Component.VC.14.44.17.14.x86.x64 ^
+ --add Microsoft.VisualStudio.Component.VC.14.44.17.14.x86.x64.Spectre ^
+ --add Microsoft.VisualStudio.Component.VC.14.44.17.14.CLI.Support ^
+ --add Microsoft.VisualStudio.Component.VC.14.44.17.14.ATL ^
+ --add Microsoft.VisualStudio.Component.VC.14.44.17.14.MFC ^
+ --add Microsoft.NetCore.Component.Runtime.8.0
 
 
 rem --quiet --norestart
--- _2_5_Settings.bat
+++ _2_5_Settings.bat
@@ -1,7 +1,7 @@
 set repoURL=https://github.com/Jason-Ma-0012/MooaToon-Engine.git
 set engineFolderName=MooaToon-Engine
-set engineBranchName=5.6
+set engineBranchName=5.7
 set projectFolderName=MooaToon-Project
-set projectBranchName=5.6_MooaToonProject
+set projectBranchName=5.7_MooaToonProject
 set PROMPT_ARGUMENT=--prompt --threads=16
 
