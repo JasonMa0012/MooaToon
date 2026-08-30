8563fa12b7cfbd1ee0dfda253744371b4431c1e5
jason ma <jasonma@foxmail.com>
2026-08-31 01:58:15 +0800

    Fix MooaToon debug project launcher

--- _5_0_Run_MooaToon_Project_Debug.bat
+++ _5_0_Run_MooaToon_Project_Debug.bat
@@ -1,12 +1,44 @@
 @echo off
+setlocal
 
 echo ============================================================
 echo =                Running MooaToon Project                  =
 echo ============================================================
 
 
-call _2_5_Settings.bat
+set "scriptRoot=%~dp0"
+pushd "%scriptRoot%" || (
+    echo Failed to switch to the MooaToon root directory: "%scriptRoot%"
+    exit /b 1
+)
+
+call "%scriptRoot%_2_5_Settings.bat"
+if errorlevel 1 (
+    echo Failed to load MooaToon settings.
+    popd
+    exit /b 1
+)
+
+set "editorExe=%scriptRoot%%engineFolderName%\Engine\Binaries\Win64\UnrealEditor-Win64-Debug.exe"
+set "projectFile=%scriptRoot%%projectFolderName%\MooaToon_Project.uproject"
+
+if not exist "%editorExe%" (
+    echo UnrealEditor Debug executable was not found: "%editorExe%"
+    popd
+    exit /b 1
+)
+
+if not exist "%projectFile%" (
+    echo MooaToon project file was not found: "%projectFile%"
+    popd
+    exit /b 1
+)
 
 @echo on
 
-%engineFolderName%\Engine\Binaries\Win64\UnrealEditor-Win64-Debug.exe %cd%\%projectFolderName%\MooaToon_Project.uproject
+"%editorExe%" "%projectFile%" -log
+set "exitCode=%errorlevel%"
+
+@echo off
+popd
+endlocal & exit /b %exitCode%
