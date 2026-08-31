b0cff46199403e79c04c63bf490a646dd07879e7
jason ma <jasonma@foxmail.com>
2026-08-31 14:43:29 +0800

    Fix Debug launcher project paths

--- _4_0_Build_And_Run_MooaToon_Debug.bat
+++ _4_0_Build_And_Run_MooaToon_Debug.bat
@@ -1,5 +1,6 @@
 @echo off
-call _2_5_Settings.bat
-call %engineFolderName%\GenerateProjectFiles.bat
-call %engineFolderName%\Engine\Build\BatchFiles\Build.bat -Target="UnrealEditor Win64 Debug" -Target="ShaderCompileWorker Win64 Debug -Quiet" -WaitMutex -FromMsBuild
-%engineFolderName%\Engine\Binaries\Win64\UnrealEditor-Win64-Debug.exe %projectFolderName%\MooaToon_Project.uproject -log
+set "scriptRoot=%~dp0"
+call "%scriptRoot%_2_5_Settings.bat"
+call "%scriptRoot%%engineFolderName%\GenerateProjectFiles.bat"
+call "%scriptRoot%%engineFolderName%\Engine\Build\BatchFiles\Build.bat" -Target="UnrealEditor Win64 Debug" -Target="ShaderCompileWorker Win64 Debug -Quiet" -WaitMutex -FromMsBuild
+"%scriptRoot%%engineFolderName%\Engine\Binaries\Win64\UnrealEditor-Win64-Debug.exe" "%scriptRoot%%projectFolderName%\MooaToon_Project.uproject" -log
--- _5_0_Run_MooaToon_Project_Debug.bat
+++ _5_0_Run_MooaToon_Project_Debug.bat
@@ -1,3 +1,4 @@
 @echo off
-call _2_5_Settings.bat
-%engineFolderName%\Engine\Binaries\Win64\UnrealEditor-Win64-Debug.exe %projectFolderName%\MooaToon_Project.uproject -log
+set "scriptRoot=%~dp0"
+call "%scriptRoot%_2_5_Settings.bat"
+"%scriptRoot%%engineFolderName%\Engine\Binaries\Win64\UnrealEditor-Win64-Debug.exe" "%scriptRoot%%projectFolderName%\MooaToon_Project.uproject" -log
