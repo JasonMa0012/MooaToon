dd32dc3499d95a62a74fb4cacebbcfef364ec44e
jason ma <jasonma@foxmail.com>
2026-08-31 10:23:24 +0800

    Simplify debug build and launch scripts

--- _4_0_Build_And_Run_MooaToon_Debug.bat
+++ _4_0_Build_And_Run_MooaToon_Debug.bat
@@ -1,31 +1,5 @@
 @echo off
-setlocal
-
-echo ============================================================
-echo ==          Building and Running MooaToon Debug           ==
-echo ============================================================
-
-set "scriptRoot=%~dp0"
-pushd "%scriptRoot%"
-
-call "%scriptRoot%_2_5_Settings.bat"
-if errorlevel 1 goto :failed
-
-echo Generating Unreal project files...
-call "%scriptRoot%%engineFolderName%\GenerateProjectFiles.bat"
-if errorlevel 1 goto :failed
-
-echo Building UnrealEditor and ShaderCompileWorker Debug targets...
-call "%scriptRoot%%engineFolderName%\Engine\Build\BatchFiles\Build.bat" -Target="UnrealEditor Win64 Debug" -Target="ShaderCompileWorker Win64 Debug -Quiet" -WaitMutex -FromMsBuild
-if errorlevel 1 goto :failed
-
-echo Running MooaToon Project with hardware Ray Tracing enabled...
-call "%scriptRoot%%engineFolderName%\Engine\Binaries\Win64\UnrealEditor-Win64-Debug.exe" "%scriptRoot%%projectFolderName%\MooaToon_Project.uproject" -log
-set "exitCode=%errorlevel%"
-popd
-exit /b %exitCode%
-
-:failed
-set "exitCode=%errorlevel%"
-popd
-exit /b %exitCode%
+call _2_5_Settings.bat
+call %engineFolderName%\GenerateProjectFiles.bat
+call %engineFolderName%\Engine\Build\BatchFiles\Build.bat -Target="UnrealEditor Win64 Debug" -Target="ShaderCompileWorker Win64 Debug -Quiet" -WaitMutex -FromMsBuild
+%engineFolderName%\Engine\Binaries\Win64\UnrealEditor-Win64-Debug.exe %projectFolderName%\MooaToon_Project.uproject -log
